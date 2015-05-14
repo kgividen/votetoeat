041727823ba2ff61b711de195f8f7be68a3d64cf
Kent <register@gividen.com>
2015-05-13 20:55:13 -0600

    Fixed callback when auth doesn't match.

--- auth/passport-config.js
+++ auth/passport-config.js
@@ -18,7 +18,7 @@ module.exports = function() {
                 }
 
                 if (!same) {
-                    return (null, null);
+                    return next(null, null);
                 }
 
                 next(null, user);
