908cf706c54a5f4867074487b2f1a1bf319acf1e
Kent <register@gividen.com>
2015-05-15 10:48:09 -0600

    Added some more examples of how to do some tests.

--- e2e-tests/index-spec.js
+++ e2e-tests/index-spec.js
@@ -1,11 +1,39 @@
 'use strict';
 //*******************NOTES*******************
-//
 // browser.driver.whatever is how you use the native web driver when angular is not involved.
 // element(by.model('todoList.todoText')).sendKeys('write a protractor test') is how you do it if angular is involved;
 // To Try: expect(loginNameInputElm.isPresent()).toBeTruthy();
 //*******************NOTES*******************
 
+
+describe('Main Page', function() {
+    it('should display welcome message with the first name', function () {
+        browser.get('/main');
+        expect(element(by.binding('userName')).getText()).toEqual('Hello Kent!');
+    });
+
+    it('should create group button and join button', function () {
+        browser.get('/main');
+
+        element.all(by.css('.btn')).then(function(elements) {
+            var createGroup = elements[0];
+            var joinGroup = elements[1];
+            expect(createGroup.getText()).toEqual("Create Group »");
+            expect(joinGroup.getText()).toEqual("Join Group »");
+        });
+    });
+
+    it('should open create group modal when create group button is clicked', function () {
+        element.all(by.css('.btn')).then(function(elements) {
+            var createGroup = elements[0];
+            createGroup.click();
+            expect(element(by.model('groupName')).isPresent()).toBe(true);
+
+        });
+    })
+});
+
+//NOTE: This would test the index page but we are auto logging in via the protractor.conf.js so this doesn't work.
 //describe('VoteToEat Index Page', function() {
 //    it('should have a title', function () {
 //        browser.driver.get('http://localhost:3000');
@@ -24,6 +52,36 @@
 //});
 
 
+
+
+
+//*******************Examples*******************
+//HERE IS AN EXAMPLE OF HOW TO GET MULTIPLE ITEMS FROM AN ARRAY in a MODEL
+    //var todoList = element.all(by.repeater('todo in todoList.todos'));
+    //expect(todoList.count()).toEqual(3);
+    //expect(todoList.get(2).getText()).toEqual('write a protractor test');
+//*******************Examples*******************
+
+//*******************Example using PAGES*******************
+//var MainPage = function() {
+//    this.greeting = element(by.binding('userName'));
+//
+//    this.get = function() {
+//        browser.get('http://localhost:3000/main');
+//    };
+//
+//};
+//
+//describe('Main Page', function() {
+//    it('should display welcome message with the first name', function () {
+//        var mainPage = new MainPage();
+//        mainPage.get();
+//        expect(mainPage.greeting.getText()).toEqual('Hello Kent!');
+//    });
+//});
+//
+
+
 //describe('Login in and out of Vote to eat', function() {
 //    it('should change the page and then logout', function () {
 //        browser.driver.get('http://localhost:3000');
@@ -35,36 +93,4 @@
 //        expect(browser.driver.findElement(by.id('submit-login')).getAttribute('value')).toMatch('Log in');
 //    });
 //});
-
-describe('Main Page', function() {
-    it('should display welcome message with the first name', function () {
-        browser.get('/main');
-        expect(element(by.binding('userName')).getText()).toEqual('Hello Kent!');
-    });
-
-    it('should create group button and join button', function () {
-        browser.get('/main');
-
-        //HERE IS AN EXAMPLE OF HOW TO GET MULTIPLE ITEMS
-        //var todoList = element.all(by.repeater('todo in todoList.todos'));
-        //expect(todoList.count()).toEqual(3);
-        //expect(todoList.get(2).getText()).toEqual('write a protractor test');
-        //
-        //
-        element.all(by.css('.btn')).then(function(elements) {
-            var createGroup = elements[0];
-            var joinGroup = elements[1];
-            expect(createGroup.getText()).toEqual("Create Group »");
-            expect(joinGroup.getText()).toEqual("Join Group »");
-        });
-    });
-
-    it('should open create group modal when create group button is clicked', function () {
-        element.all(by.css('.btn')).then(function(elements) {
-            var createGroup = elements[0];
-            createGroup.click();
-            expect(element(by.model('groupName')).isPresent()).toBe(true);
-
-        });
-    })
-});
+//*******************Example using PAGES*******************
