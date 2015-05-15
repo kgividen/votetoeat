a7cb4e4d5929bb9ce79ef22a91e6c9612bf63b64
Kent <register@gividen.com>
2015-05-15 10:28:15 -0600

    Added a comment on how to not restrict the urls. Added a ! to login.

--- routes/main.js
+++ routes/main.js
@@ -3,6 +3,7 @@ var router = express.Router();
 var restrict = require('../auth/restrict');
 
 router.get('/', restrict, function(req, res, next) {
+//router.get('/', function(req, res, next) {
     var vm = {
         title: 'Main',
         firstName: req.user ? req.user.firstName : null
