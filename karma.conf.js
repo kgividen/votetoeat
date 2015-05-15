da10d7351a645960e62c0d92629e629d0a5a6bcc
Kent <register@gividen.com>
2015-05-15 13:27:20 -0600

    Added firefox for karma.

--- karma.conf.js
+++ karma.conf.js
@@ -65,8 +65,7 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Chrome'],
-
+    browsers: ['Chrome', 'Firefox'],
       plugins : [
           'karma-chrome-launcher',
           'karma-firefox-launcher',
--- unit-tests/MainPage-spec.js
+++ unit-tests/MainPage-spec.js
@@ -1,11 +1,10 @@
 'use strict';
 
 describe('Main Page', function() {
-
     beforeEach(module('app'));
 
     describe('MainPage', function(){
-        it('should ....', inject(function($controller) {
+        it('should have a MainController defined', inject(function($controller) {
             //spec body
             var mainController = $controller('MainController');
             expect(mainController).toBeDefined();
@@ -14,7 +13,7 @@ describe('Main Page', function() {
     });
 });
 
-describe("Main Page ", function() {
+describe("Main Page Controller", function() {
     beforeEach(module("app"));
     var MainController,
         scope;
@@ -26,8 +25,35 @@ describe("Main Page ", function() {
         });
     }));
 
-    it('SuggestionTitle says Suggestions', function () {
+    it('should have scope variables set to defaults', function () {
+        expect(scope.users).toEqual([]);
+        expect(scope.places).toEqual([]);
+        expect(scope.vote_max).toEqual(10);
+        expect(scope.voted).toEqual(false);
+        expect(scope.showMainApp).toEqual(false);
+        expect(scope.groupName).toEqual("");
+        expect(scope.voteBtnActive).toEqual(0);
+        expect(scope.businessData).toEqual([]);
+        expect(scope.location).toEqual("");
         expect(scope.suggestionTitle).toEqual("Suggestions");
+        expect(scope.currentSuggestionGroup).toEqual("yelp");
+        expect(scope.showSetUserName).toEqual(false);
+        expect(scope.joinOrCreateBtn).toEqual("create");
+        expect(scope.dealsOnly).toEqual(false);
+        expect(scope.yelpOffset).toEqual(0);
+        expect(scope.yelpSearchType).toEqual("cll");
+        expect(scope.yelpSortType).toEqual(0);
+        expect(scope.yelpNextDisabled).toEqual(false);
+        expect(scope.googleSortType).toEqual(0);
+        expect(scope.showLoading).toEqual(true);
     });
 
 });
+
+//********************NOTES***********************
+    //skip me
+    //xdescribe('Main Page', function() {
+    //    xit('should skipme', inject(function($controller) {
+    //    }));
+    //});
+//********************NOTES***********************
