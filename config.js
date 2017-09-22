649529ea86632afeed86551b15c315c9bfbfd214
Kent Gividen <kgividen@gmail.com>
2017-09-22 12:05:01 -0600

    resetting to v2

--- config.js
+++
@@ -1,7 +0,0 @@
-// var mongoFilePath = (process.env.NODE_ENV === 'production') ? '../mongoDBFiles' : 'mongoDBFiles';
-
-module.exports = {
-    serverPort: 3000,
-    // mongoFilePath: mongoFilePath,
-    cookieMaxAge: 30 * 24 * 3600 * 1000
-};
